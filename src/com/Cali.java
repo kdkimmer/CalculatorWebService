c882088d271dd496c3bcab846667019b79bd646f
Druessel <DruesselK@corp.saic.com>
2015-12-21 09:14:48 -0600

    Add services

---
+++ src/com/Cali.java
@@ -0,0 +1,19 @@
+package com;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+
+@WebService
+public class Cali {
+	
+	@WebMethod
+	public int addition(int p1, int p2){
+		return p1 + p2;
+	}
+	@WebMethod
+	public int subtraction(int p1, int p2){
+		return p1 - p2;
+	}
+
+
+}
